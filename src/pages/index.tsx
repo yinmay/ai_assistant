dd7f496dce28d55be64ee28639a1500d60c694e3
yinmay <1102316727@qq.com>
2024-12-28 14:03:21 -0500

    Refactor configuration and simplify index page layout
    - Updated .prettierrc.json for consistent formatting.
    - Removed unused color definitions from tailwind.config.ts.
    - Simplified the Home component in index.tsx by reducing unnecessary markup.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,16 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import Image from 'next/image';
+import { Geist, Geist_Mono } from 'next/font/google';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-     test
-     
-    </div>
-  );
+  return <div>test</div>;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,6 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
     },
   },
   plugins: [],
