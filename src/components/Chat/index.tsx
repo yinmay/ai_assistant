0e6905a1e23ebcd18d51fc192de273560224c7e3
yinmay <1102316727@qq.com>
2024-12-30 21:06:37 -0500

    Enhance Chat component with chat logs management and AI response handling
    - Integrated chat storage utilities to manage chat logs in local storage.
    - Updated Chat component to display chat history and user interactions.
    - Improved user experience by handling AI responses and loading states.
    - Refactored code for better readability and maintainability.

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -3,48 +3,82 @@ import { getCompletion } from "@/utils/getCompletion";
 import { ActionIcon, Textarea } from "@mantine/core";
 import { IconSend, IconEraser } from "@tabler/icons-react";
 import { ChatLogsType } from "@/types";
+import {
+    clearChatLogs,
+    getChatLogs,
+    updateChatLogs,
+} from "@/utils/chatStorage";
+import clsx from "clsx";
+const LOCAL_KEY = "ai_demo";
 
 
 export default function Chat() {
     const [prompt, setPrompt] = useState("");
     const [loading, setLoading] = useState(false);
     const [completion, setCompletion] = useState<string>("");
-  const [chatList, setChatList] = useState<ChatLogsType>([]);
+    const [chatList, setChatList] = useState<ChatLogsType>([]);
 
-   const setChatLogs = (logs: ChatLogsType) => {
-    setChatList(logs);
-    // updateChatLogs(LOCAL_KEY, logs);
-  };
+    const setChatLogs = (logs: ChatLogsType) => {
+        setChatList(logs);
+        updateChatLogs(LOCAL_KEY, logs);
+    };
 
 
-      const getAIResp = async () => {
-    setLoading(true);
-    const list = [
-      ...chatList,
-      {
-        role: "user",
-        content: prompt,
-      },
-    ];
-    setChatLogs(list);
-    const resp = await getCompletion({
-      prompt: prompt,
-      history: chatList.slice(-4),
-    });
-    setPrompt("");
-    setCompletion(resp.content);
-    setChatLogs([
-      ...list,
-      {
-        role: "assistant",
-        content: resp.content,
-      },
-    ]);
-    setLoading(false);
-  };
+    const getAIResp = async () => {
+        setLoading(true);
+        const list = [
+            ...chatList,
+            {
+                role: "user",
+                content: prompt,
+            },
+        ];
+        setChatLogs(list);
+        const resp = await getCompletion({
+            prompt: prompt,
+            history: chatList.slice(-4),
+        });
+        setPrompt("");
+        setCompletion(resp.content);
+        setChatLogs([
+            ...list,
+            {
+                role: "assistant",
+                content: resp.content,
+            },
+        ]);
+        setLoading(false);
+    };
     return (
         <div className="h-screen flex flex-col items-center">
-            <div className="w-3/5 max-w-md">{completion}</div>
+            <div className="w-3/5 max-w-md">{chatList.map((item, idx) => (
+          <div
+            key={`${item.role}-${idx}`}
+            className={clsx(
+              {
+                flex: item.role === "user",
+                "flex-col": item.role === "user",
+                "items-end": item.role === "user",
+              },
+              "mt-4",
+            )}
+          >
+            <div>{item.role}</div>
+            <div
+              className={clsx(
+                "rounded-md",
+                "shadow-md",
+                "px-4",
+                "py-2",
+                "mt-1",
+                "w-full",
+                "max-w-4xl",
+              )}
+            >
+              {item.content}
+            </div>
+          </div>
+        ))}</div>
             <div className="flex items-center w-full">
                 <ActionIcon
                     className="mr-2"
@@ -64,7 +98,7 @@ export default function Chat() {
                 <ActionIcon
                     className="ml-2"
                     loading={loading}
-                  onClick={() => getAIResp()}
+                    onClick={() => getAIResp()}
                 >
                     <IconSend></IconSend>
                 </ActionIcon>
